80b3c072c902ba7b459ca2f4c69948cf4e79a3a9
qianlongzt <1401300561@qq.com>
2024-09-21 07:15:27 +0800

    fix(reap): forward signal

--- reaper.go
+++ reaper.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/signal"
 	"syscall"
 
 	reaper "github.com/ramr/go-reaper"
@@ -14,7 +16,7 @@ func forkExec() {
 
 	pwd, err := os.Getwd()
 	if err != nil {
-		logrus.Fatalf("Failed to get current working directory: %s", err)
+		logrus.Fatalf("Failed to get current working directory: %s", err.Error())
 		return
 	}
 
@@ -31,17 +33,40 @@ func forkExec() {
 	}
 	pid, err := syscall.ForkExec(os.Args[0], os.Args, pattrs)
 	if err != nil {
-		logrus.Fatalf("Failed to fork exec: %s", err)
+		logrus.Fatalf("Failed to fork exec: %s", err.Error())
 		return
 	}
 
+	signalToFork(pid)
+
 	_, err = syscall.Wait4(pid, &wstatus, 0, nil)
 	for syscall.EINTR == err {
+		fmt.Println("wait4 got ", err.Error())
 		_, err = syscall.Wait4(pid, &wstatus, 0, nil)
 	}
+
 	if err != nil {
-		logrus.Fatalf("Failed to wait: %s", err)
+		// FIXME: may be reaped by reaper.....
+		// if err != syscall.ECHILD {
+		logrus.Errorf("Failed to wait: %s", err.Error())
+		// }
 		return
 	}
 	os.Exit(wstatus.ExitStatus())
+}
+
+func signalToFork(pid int) {
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		logrus.Fatalf("Failed findProcess pid:%d,%s", pid, err.Error())
+	}
+	termChan := make(chan os.Signal, 1)
+	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR2)
+	go func() {
+		s := <-termChan
+		logrus.Infof("Got signal: %v,%s", s, s.String())
+		if err := p.Signal(s); err != nil {
+			logrus.Errorf("Failed to send signal to child: %s", err.Error())
+		}
+	}()
 }
