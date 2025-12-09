dd23cfdfc651c9f1b67983fdcd0dd32d5ee1a151
XiaoFlins <flins@189.cn>
2025-12-09 16:57:12 +0800

    support loong64

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p dist
 export GOOS="linux"
 export CGO_ENABLED=0
 VERSION=${1:-$(git describe --tags --always --dirty)}
-for arch in amd64 386 arm arm64; do GOARCH="$arch" go build -ldflags="-X 'main.Version=$VERSION'" && file supercronic | grep 'statically linked' && mv supercronic "dist/supercronic-${GOOS}-${arch}"; done
+for arch in amd64 386 arm arm64 loong64; do GOARCH="$arch" go build -ldflags="-X 'main.Version=$VERSION'" && file supercronic | grep 'statically linked' && mv supercronic "dist/supercronic-${GOOS}-${arch}"; done
 pushd dist
 ls -lah *
 file *
